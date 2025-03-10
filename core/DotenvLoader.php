a86d6b782922c831d4ee079e9abc460ee94bc1b1
richazim <saibouazim90@gmail.com>
2025-03-10 07:15:30 +0000

    Dotenv File Loader

---
+++ core/DotenvLoader.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace SousControle\Core;
+
+class DotenvLoader
+{
+    public function load(): void
+    {
+        $array = file('./../.env'); // get .env lines as an array
+        $array = $this->removeNonValidArrayCases($array); 
+        foreach($array as $line){
+            $line = $this->removeComment($line);
+
+            if($line){
+                [$key, $value] = explode('=', $line);
+                $_ENV[trim($key)] = trim($value);
+            }
+            
+        } 
+    }
+
+    private function removeNonValidArrayCases(array $array): array|null // ex: 1 => "\n" inside an array // or ex: 1 => "# someText"
+    {
+        $spaces = [" ", "\t", "\n", "\r"];
+
+        foreach($array as $key => $value){
+            if(in_array($value, $spaces) || preg_match('/^\s*#.*$/', $value)){
+                unset($array[$key]);
+            }
+        } 
+        
+        return $array;
+    }
+
+    private function removeComment(string $value): string
+    {
+        $pattern = '/#.*/';
+        $remplacement = '';
+
+        $resultat = preg_replace($pattern, $remplacement, $value);
+        return $resultat;
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,4 +1,11 @@
-<?php
+<?php 
+
+use SousControle\Core\DotenvLoader;
 
 require __DIR__ . "/../vendor/autoload.php";
+require_once __DIR__ . "/../core/helpers/arrays.php";
 
+$dotenv = new DotenvLoader();
+$dotenv->load(); 
+
+dump($_ENV);
